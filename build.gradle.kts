5e9212a1428a820465a6ae63a459bc730a73e054
Oli Zimpasser <oglimmer@gmail.com>
2020-11-22 01:52:55 +0100

    Add Jacoco

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("io.spring.dependency-management") version "1.0.10.RELEASE"
     kotlin("jvm") version "1.4.10"
     kotlin("plugin.spring") version "1.4.10"
+    id("jacoco")
 }
 
 group = "de.oglimmer"
@@ -53,6 +54,46 @@ tasks.withType<KotlinCompile> {
         jvmTarget = "11"
     }
 }
+
+tasks.getByName<JacocoReport>("jacocoTestReport") {
+    reports {
+        xml.isEnabled = true
+        html.isEnabled = true
+    }
+    afterEvaluate {
+        classDirectories.from(files(classDirectories.files.map {
+            fileTree(it) {
+                exclude("**/config/**", "**/entity/**")
+            }
+        }))
+    }
+}
+
+//tasks.create("coverageReport") {
+//    dependsOn(tasks.getByName<JacocoReport>("jacocoTestReport"))
+//
+//    val reportFile = project.file("build/reports/jacoco/test/jacocoTestReport.xml")
+//    inputs.file(reportFile)
+//
+//    doLast {
+//        val slurper = groovy.util.XmlSlurper()
+//        slurper.setFeature("http://apache.org/xml/features/disallow-doctype-decl", false)
+//        slurper.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false)
+//        val xml = slurper.parse(reportFile)
+//        val counter = xml.counter.find { node ->
+//            node.@ type == 'BRANCH'
+//        }
+//        val missed = counter.@ missed . toDouble ()
+//        val covered = counter.@ covered . toDouble ()
+//        val total = missed + covered
+//        val percentage = covered / total * 100
+//
+//        println(String.format("Missed %.0f branches%n", missed))
+//        println(String.format("Covered %.0f branches%n", covered))
+//        println(String.format("Total %.0f%%%n", percentage))
+//    }
+//}
+
 
 //tasks.create<Test>("integrationTest") {
 //    description = "Runs the integration tests."
